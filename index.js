8ff18fae9b6e384dfee6302cb954d4dab9ed9be1
Surya <surya.vme005@gmail.com>
2024-06-16 21:21:19 +0530

    modified

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-im
+import express from 'express';
+import { dirname }  from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res)=>{
+    res.render("index.ejs");
+})
+
+app.listen(port,(req, res)=>{
+    console.log(`Listening server on port : ${port}`);
+})
