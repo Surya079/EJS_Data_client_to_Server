296e7bf202f83bc4442e2286b6830daf881e2b59
Surya <surya.vme005@gmail.com>
2024-06-16 22:20:05 +0530

    modified

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 import express from 'express';
-import { dirname }  from 'path';
-import { fileURLToPath } from 'url';
+// import bodyParser from 'body-parser';
+// import { dirname }  from 'path';
+// import { fileURLToPath } from 'url';
 
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({extended : true}))
+
 app.get('/', (req, res)=>{
+
     res.render("index.ejs");
+
 })
 
+app.post("/find",(req, res)=>{
+    let name = req.body["firstname"] + req.body["secondname"];
+    let len = name.length;
+    res.render("index.ejs",len)
+    
+});
+
 app.listen(port,(req, res)=>{
+
     console.log(`Listening server on port : ${port}`);
+
 })
